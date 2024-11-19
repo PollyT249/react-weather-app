1a9baeea9ce5cdfc5c7115fb005dfa895282fd3c
PollyT249 <polinatretiakova249@gmail.com>
2024-11-19 21:59:46 +0200

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,26 +11,34 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-warning" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-warning w-100"
+            />
           </div>
         </div>
       </form>
-      <h1>Tallinn</h1>
+      <h1 className="mb-3">Tallinn</h1>
       <ul>
         <li>Tuesday 18:44</li>
         <li>Light snow</li>
       </ul>
+
       <div className="row">
         <div className="col-6">
           <img
             src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/snow-night.png"
             alt="Light Snow"
           />
-          1℃|℉
+          <span className="temperature">1</span>
+          <span className="unit">℃</span>
         </div>
+
         <div className="col-6">
           <ul>
             <li>Precipitation: 15%</li>
