57347de293d043335a16c3a123f8156b07fce178
PollyT249 <polinatretiakova249@gmail.com>
2024-11-20 21:10:38 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Tallinn" />
         <footer>
           This project was coded by Polina Tretiakova and{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,52 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      date: "Tuesday 18:44",
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      iconUrl:
+        "https://shecodes-assets.s3.amazonaws.com/api/weather/icons/snow-day.png",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-warning w-100"
+              />
+            </div>
+          </div>
+        </form>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-warning w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1 className="mb-3">Tallinn</h1>
-      <ul>
-        <li>Tuesday 18:44</li>
-        <li>Light snow</li>
-      </ul>
+          <div className="col-6">
+            <h1 className="mb-3">
+              {weatherData.city}, {weatherData.date}
+            </h1>
 
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/snow-night.png"
-            alt="Light Snow"
-          />
-          <span className="temperature">1</span>
-          <span className="unit">℃</span>
-        </div>
+            <div className="text-capitalize">{weatherData.description}</div>
+            <ul>
+              <li>Humidity: {Math.round(weatherData.humidity)} %</li>
+              <li>Wind: {Math.round(weatherData.wind)} m/sec</li>
+            </ul>
+          </div>
 
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 93%</li>
-            <li>Wind: 3km/h</li>
-          </ul>
+          <div className="col-6">
+            <img src={weatherData.iconUrl} alt={weatherData.description} />
+
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">℃</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "2d96d64425dca1d6eda00d942a281c0d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return <p>Loading...</p>;
+  }
 }
